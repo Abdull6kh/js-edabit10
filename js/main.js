058a30064c8637524ab408ce8630bf89c49e1fbe
Abdull6kh <pbgpbg016@gmail.com>
2022-06-16 23:20:46 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// --------------------------1-----------------------------
+function addition(a, b) {
+	return a + b
+}
+// --------------------------2-----------------------------
+function convert(minutes) {
+	return minutes * 60
+}
+// --------------------------3-----------------------------
+function addition(num) {
+	return ++num
+}
+// --------------------------4-----------------------------
+function triArea(base, height) {
+	return (base * height) / 2
+}
+// --------------------------5-----------------------------
+function calcAge(age) {
+	return age * 365
+}
+// --------------------------6-----------------------------
+function remainder(x, y) {
+	return x % y
+}
+// --------------------------7-----------------------------
+function footballPoints(wins, draws, losses) {
+	return (wins * 3) + draws
+}
+// --------------------------8-----------------------------
+function points(twoPointers, threePointers) {
+	return (twoPointers * 2) + (threePointers * 3)
+}
+// --------------------------9-----------------------------
+function lessThan100(a, b) {
+	if(a + b < 100){
+		return true
+	}else {
+		return false
+	}
+}
+// --------------------------10----------------------------
+function animals(chickens, cows, pigs) {
+	return (chickens * 2) + (cows * 4) + (pigs * 4)
+}
